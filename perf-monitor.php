3379ca3668fd6aad0ac4cc271df149540aa01388
Ihor Vorotnov <ihor.vorotnov@gmail.com>
2018-03-29 20:03:13 +0300

    Update plugin file header

--- perf-monitor.php
+++ perf-monitor.php
@@ -4,11 +4,11 @@
  *
  * A handy way to monitor your website performance.
  *
- * @author            Ihor Vorotnov <ihor.vorotnov@gmail.com>
- * @copyright         2018 Ihor Vorotnov, PE
- * @license           GPL-2.0+
  * @package           PerfMonitor
- * @since             0.1.0
+ * @link              https://github.com/ihorvorotnov/perf-monitor/
+ * @author            Ihor Vorotnov <ihor.vorotnov@gmail.com>
+ * @copyright         1999-2018 Ihor Vorotnov, PE
+ * @license           GPL-2.0+
  *
  * @wordpress-plugin
  * Plugin Name:       Performance Monitor
@@ -23,6 +23,16 @@
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       perf-monitor
  * Domain Path:       /languages
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  */
 
 if ( ! defined( 'WPINC' ) ) {
