e895b0c2ba409c91216c61fe1be38e28c1594a01
Ihor Vorotnov <ihor.vorotnov@gmail.com>
2018-03-29 18:09:29 +0300

    Introduce main plugin file

---
+++ perf-monitor.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Performance Monitor
+ *
+ * A handy way to monitor your website performance.
+ *
+ * @author            Ihor Vorotnov <ihor.vorotnov@gmail.com>
+ * @copyright         2018 Ihor Vorotnov, PE
+ * @license           GPL-2.0+
+ * @package           PerfMonitor
+ * @since             0.1.0
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Performance Monitor
+ * Plugin URI:        https://github.com/ihorvorotnov/perf-monitor/
+ * Support URI:       https://github.com/ihorvorotnov/perf-monitor/issues/
+ * Documentation URI: https://github.com/ihorvorotnov/perf-monitor/wiki/
+ * Description:       A handy way to monitor your website performance.
+ * Version:           0.1.0
+ * Author:            Ihor Vorotnov
+ * Author URI:        https://ihorvorotnov.com/
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       perf-monitor
+ * Domain Path:       /languages
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+define( 'PERF_MONITOR_VERSION', '0.1.0' );
