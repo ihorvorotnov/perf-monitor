64c5c2dc58008ad714b974bd123d2ba40bdd7979
Ihor Vorotnov <ihor.vorotnov@gmail.com>
2018-04-01 11:06:44 +0200

    Add extra data to output (DOM stages etc)

--- public/assets/pm.js
+++ public/assets/pm.js
@@ -6,22 +6,35 @@ window.onload = function () {
         const t = entries[0];
         console.log(t);
 
-        const data = {
+        const stages = {
             'dns': t.domainLookupEnd - t.domainLookupStart,
             'connect': t.connectEnd - t.connectStart,
             'ssl': t.secureConnectionStart === 0 ? 0 : t.connectEnd - t.secureConnectionStart,
             'wait': t.responseStart - t.requestStart,
             'receive': t.responseEnd - t.responseStart,
             'render': t.domComplete - t.responseEnd,
-            'total_response': t.responseEnd - t.requestStart,
-            'domloaded': t.domContentLoadedEventEnd,
-            'loaded': t.loadEventEnd
+            'total_response': t.responseEnd - t.requestStart
         };
 
-        Object.entries(data).forEach(
+        Object.entries(stages).forEach(
             function ([key, value]) {
                 let timeCol = document.getElementById(`pm_${key}_time`);
                 timeCol.innerHTML = value.toFixed(1) + '<span>ms</span>';
+            }
+        );
+
+        const events = {
+            'first_byte': t.responseStart,
+            'dom_interactive': t.domInteractive,
+            'dom_content_loaded': t.domContentLoadedEventEnd,
+            'dom_complete': t.domComplete,
+            'loaded': t.loadEventEnd
+        };
+
+        Object.entries(events).forEach(
+            function ([key, value]) {
+                let timeCol = document.getElementById(`pm_${key}_time`);
+                timeCol.innerHTML = '<span>@</span>' + value.toFixed(1) + '<span>ms</span>';
             }
         );
     }, 10);
